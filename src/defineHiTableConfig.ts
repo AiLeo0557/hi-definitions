28f1853b8a5715c3fc618d367ba5143f8bcd0a39
AiLeo0557 <leo1949@dingtalk.com>
2025-04-16 15:33:02 +0800

    feat: 增强 HiTableConfig 和 HiFormConfig 接口的类型注释以提高代码可读性和类型安全

--- src/defineHiTableConfig.ts
+++ src/defineHiTableConfig.ts
@@ -1,6 +1,54 @@
-import { reactive, ref, watch } from "@vue/runtime-core"
-import { defineHiTableColConfig, type HiTableColOptions } from "./defineHiTableColConfig"
-import { getFieldValueByPath } from "hi-utils-pro"
+import { reactive, Ref, ref, watch } from "@vue/runtime-core"
+import { defineHiTableColConfig, HiTableColElOptions, type HiTableColOptions } from "./defineHiTableColConfig"
+import { getFieldValueByPath, type HiRequestArgument } from "hi-utils-pro"
+import { defineHiFormItems, FormElementOption, HiFormItemOption } from "./defineHiFormItems"
+
+export interface HiFormConfig {
+  inline: boolean // 是否行内表单
+  defaultLength: number // 默认表单项长度
+  defaultSpan: number // 默认表单项跨度
+  items: HiFormItemOption<FormElementOption>[] // 表单项
+  formData: Record<string, any> // 表单数据
+  showSearchBtn: boolean // 是否显示搜索按钮
+  reFetch: boolean // 是否重新请求
+  onSubmit: (data: Record<string, any>, searchResult?: any) => void // 提交回调
+}
+
+export interface HiTableColConfig {
+  args?: HiRequestArgument<any> | null // 请求参数
+  default?: HiTableColElOptions[] | null // 默认值
+}
+export interface HiTableDataConfig {
+  args?: HiRequestArgument<any> | null // 请求参数
+  default?: () => Ref<Record<string, any>[]> | null // 默认值
+  from?: string // 数据来源
+}
+export interface HiTableActionsConfig {
+  items: any[] // 操作按钮
+  container_id?: string // 容器ID
+  items_space?: number // 操作按钮间距
+}
+export interface HiTableConfig {
+  name: string // 表格名称
+  label: string // 表格标题
+  sub_label: string // 表格副标题
+  moduleId: string // 模块ID 用于请求服务端 colummns
+  rowKey: string // 表格行key
+  selected_data_model: string // 选中数据模型
+  hide_selection_col: boolean // 是否隐藏选择列
+  hide_index_col: boolean // 是否隐藏索引列
+  title: string // 表格标题
+  tableHeight: string // 表格高度
+  tableWidth: string // 表格宽度
+  minHeight: string // 表格最小高度
+  searchFormConfig: HiFormConfig // 搜索表单配置
+  columns_config: HiTableColConfig // 列配置
+  data_config: HiTableColConfig // 数据配置
+  chart_config?: any // 图表配置
+  sorte_config?: any // 排序配置
+  pagination_config?: any // 分页配置
+  actions_config?: HiTableActionsConfig // 操作按钮配置
+}
 
 export function defineHiTableConfig(
   config: any,
@@ -36,9 +84,8 @@ export function defineHiTableConfig(
     sorte_config,
     minHeight
   } = config
-  const columns = ref<any>([])
+  const columns = ref<HiTableColElOptions[]>([])
   if (table_columns) {
-    // <HiTableColOptions>
     columns.value = defineHiTableColConfig(table_columns, page_state, page_states)
   }
   if (table_columns_config) {
@@ -85,10 +132,9 @@ export function defineHiTableConfig(
   if (table_search_form_config) {
     const [searchFormData, searchFormItems]: any =
       table_search_form_config?.items || []
-        ? defineHiTableColConfig(table_search_form_config?.items || [])
+        ? defineHiFormItems(table_search_form_config?.items || [])
         : []
     Object.assign(tableDataParam, searchFormData)
-    console.log(85, tableDataParam)
     Object.assign(result, {
       searchFormConfig: {
         inline: true,
@@ -98,7 +144,7 @@ export function defineHiTableConfig(
         formData: searchFormData,
         showSearchBtn: true,
         reFetch: true,
-        async onSubmit(data: any, searchResult?: any) {
+        async onSubmit(data: Record<string, any>, searchResult?: any) {
           if (table_search_form_config?.on_search_cb_config) {
             const { set_formdata_key_name, set_formdata_key_value } =
               table_search_form_config?.on_search_cb_config
